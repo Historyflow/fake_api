32e6f2e12f010063ac2793f953b198529f040f3f
soal <sorrow.about.alice@gmail.com>
2017-04-20 01:48:13 +0300

    Dicts & updated shapes

--- server.js
+++ server.js
@@ -19,15 +19,15 @@ app.use(function(req, res, next) {
   next();
 });
 app.get("/api/dicts", function(req, res) {
-  res.sendFile("test_data_new/dicts.json", options)
+  res.sendFile("test_data_new/dicts.json", options);
 });
-app.get("/api/contexts/2000", function(req, res) {
-  res.sendFile("test_data/jsons/contexts/second_punic_war_context.json", options);
+app.get("/api/public/scenarios/second_punic_war", function(req, res) {
+  res.sendFile("test_data/jsons/contexts/second_punic_war.json", options);
 });
-app.get("/api/contexts/0", function(req, res) {
+app.get("/api/public/scenarios/0", function(req, res) {
   res.sendFile("test_data/jsons/contexts/world_hist_process.json", options);
 });
-app.get("/api/elements", function(req, res, next) {
+app.get("/api/public/elements", function(req, res, next) {
   var output = { meta: {}, data: [] };
   if (req.query.ids) {
     var files = [
@@ -52,94 +52,94 @@ app.get("/api/elements", function(req, res, next) {
   }
 });
 
-app.get("/api/search/elements", function(req, res) {
+app.get("/api/public/search/elements/:name", function(req, res) {
   res.sendFile(`test_data/jsons/${req.query.name}.json`, options);
 });
 
-app.get("/api/elements/100", function(req, res) {
+app.get("/api/public/elements/100", function(req, res) {
   res.sendFile("test_data/jsons/second_punic_war.json", options);
 });
-// app.get("/api/elements/100/text", function(req, res) {
+// app.get("/api/public/elements/100/text", function(req, res) {
 //   res.sendFile("test_data/jsons/second_punic_war_text.json", options);
 // });
 
-app.get("/api/elements/1", function(req, res) {
+app.get("/api/public/elements/1", function(req, res) {
   res.sendFile("test_data/jsons/roman_republic.json", options);
 });
-// app.get("/api/elements/1/text", function(req, res) {
+// app.get("/api/public/elements/1/text", function(req, res) {
 //   res.sendFile("test_data/jsons/roman_republic_text.json", options);
 // });
 
-app.get("/api/elements/7", function(req, res) {
+app.get("/api/public/elements/7", function(req, res) {
   res.sendFile("test_data/jsons/carthago.json", options);
 });
-// app.get("/api/elements/7/text", function(req, res) {
+// app.get("/api/public/elements/7/text", function(req, res) {
 //   res.sendFile("test_data/jsons/carthago_text.json", options);
 // });
 
-app.get("/api/elements/9001", function(req, res) {
+app.get("/api/public/elements/9001", function(req, res) {
   res.sendFile("test_data/jsons/battle_of_cannae.json", options);
 });
-// app.get("/api/elements/9001/text", function(req, res) {
+// app.get("/api/public/elements/9001/text", function(req, res) {
 //   res.sendFile("test_data/jsons/battle_of_cannae_text.json", options);
 // });
 
-app.get("/api/elements/9002", function(req, res) {
+app.get("/api/public/elements/9002", function(req, res) {
   res.sendFile("test_data/jsons/battle_of_the_trebia.json", options);
 });
-// app.get("/api/elements/9002/text", function(req, res) {
+// app.get("/api/public/elements/9002/text", function(req, res) {
 //   res.sendFile("test_data/jsons/battle_of_the_trebia_text.json", options);
 // });
 
-app.get("/api/elements/9003", function(req, res) {
+app.get("/api/public/elements/9003", function(req, res) {
   res.sendFile("test_data/jsons/battle_of_lake_trasimene.json", options);
 });
-// app.get("/api/elements/9003/text", function(req, res) {
+// app.get("/api/public/elements/9003/text", function(req, res) {
 //   res.sendFile("test_data/jsons/battle_of_lake_trasimene_text.json", options);
 // });
 
-app.get("/api/elements/9005", function(req, res) {
+app.get("/api/public/elements/9005", function(req, res) {
   res.sendFile("test_data/jsons/battle_of_zama.json", options);
 });
-// app.get("/api/elements/9005/text", function(req, res) {
+// app.get("/api/public/elements/9005/text", function(req, res) {
 //   res.sendFile("test_data/jsons/battle_of_zama_text.json", options);
 // });
 
-app.get("/api/elements/19001", function(req, res) {
+app.get("/api/public/elements/19001", function(req, res) {
   res.sendFile("test_data/jsons/hannibal_italian_route.json", options);
 });
 
-app.get("/api/elements/1018", function(req, res) {
+app.get("/api/public/elements/1018", function(req, res) {
   res.sendFile("test_data/jsons/hannibal_barka.json", options);
 });
-// app.get("/api/elements/1018/text", function(req, res) {
+// app.get("/api/public/elements/1018/text", function(req, res) {
 //   res.sendFile("test_data/jsons/hannibal_barka_text.json", options);
 // });
 
-app.get("/api/elements/1001", function(req, res) {
+app.get("/api/public/elements/1001", function(req, res) {
   res.sendFile("test_data/jsons/scipio_africanus.json", options);
 });
-// app.get("/api/elements/1001/text", function(req, res) {
+// app.get("/api/public/elements/1001/text", function(req, res) {
 //   res.sendFile("test_data/jsons/scipio_africanus_text.json", options);
 // });
 
-app.get("/api/shapes/101", function(req, res) {
+app.get("/api/public/shapes/101", function(req, res) {
   res.sendFile("test_data/shapes/battle_of_cannae.geojson", options);
 });
-app.get("/api/shapes/71", function(req, res) {
+app.get("/api/public/shapes/71", function(req, res) {
   res.sendFile("test_data/shapes/carthago.geojson", options);
 });
-app.get("/api/shapes/11", function(req, res) {
+app.get("/api/public/shapes/11", function(req, res) {
   res.sendFile("test_data/shapes/roman_republic.geojson", options);
 });
 
-app.get("/api/elements/9001/shapes", function(req, res) {
+app.get("/api/public/elements/9001/shapes", function(req, res) {
   res.sendFile("test_data/shapes/battle_of_cannae.geojson", options);
 });
-app.get("/api/elements/7/shapes", function(req, res) {
+app.get("/api/public/elements/7/shapes", function(req, res) {
   res.sendFile("test_data/shapes/carthago.geojson", options);
 });
-app.get("/api/elements/1/shapes", function(req, res) {
+app.get("/api/public/elements/1/shapes", function(req, res) {
   res.sendFile("test_data/shapes/roman_republic.geojson", options);
 });
 
